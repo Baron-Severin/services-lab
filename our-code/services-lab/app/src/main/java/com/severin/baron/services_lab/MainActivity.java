3b30e5cdee1f99a3176f01e1074bca2ce492ccc9
Baron-Severin <severinrudie@gmail.com>
2016-08-16 13:06:20 -0700

    added awareness call

--- our-code/services-lab/app/src/main/java/com/severin/baron/services_lab/MainActivity.java
+++ our-code/services-lab/app/src/main/java/com/severin/baron/services_lab/MainActivity.java
@@ -8,17 +8,25 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.awareness.Awareness;
+import com.google.android.gms.awareness.snapshot.DetectedActivityResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
 import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,GoogleApiClient.ConnectionCallbacks {
     GoogleApiClient mGoogleApiClient;
     Button activityButton, weatherButton, nearbyButton;
+    TextView activityTextView;
 
     public static final int ACTIVITY_CODE = 123;
     public static final int WEATHER_CODE = 128;
@@ -29,14 +37,24 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        activityTextView = (TextView) findViewById(R.id.activity_text_view);
+
         activityButton = (Button) findViewById(R.id.activity_button);
         weatherButton = (Button) findViewById(R.id.weather_button);
         nearbyButton = (Button) findViewById(R.id.nearby_button);
+
+        activityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivityType();
+            }
+        });
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this,this)
                 .addApi(Awareness.API)
                 .build();
+        mGoogleApiClient.connect();
 
 
     }
@@ -92,6 +110,21 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 //                    new String[]{com.google.android.gms.permission.ACTIVITY_RECOGNITION}, ACTIVITY_CODE);
 //            return;
 //        }
+
+        Awareness.SnapshotApi.getDetectedActivity(mGoogleApiClient)
+                .setResultCallback(new ResultCallback<DetectedActivityResult>() {
+                    @Override
+                    public void onResult(@NonNull DetectedActivityResult detectedActivityResult) {
+                        if (!detectedActivityResult.getStatus().isSuccess()) {
+                            Log.e("SEVTEST ", "Could not get the current activity.");
+                            return;
+                        }
+                        ActivityRecognitionResult ar = detectedActivityResult.getActivityRecognitionResult();
+                        DetectedActivity probableActivity = ar.getMostProbableActivity();
+                        Log.i("SEVTEST ", probableActivity.toString());
+                        activityTextView.setText(probableActivity.toString());
+                    }
+                });
     }
 
     private void getWeather(){
