4519aeee588b31f395325b21e2b4c49ede53a4d1
Matthew Duffin <matthewtduffin@gmail.com>
2016-08-16 12:34:03 -0700

    button refs added

--- our-code/services-lab/app/src/main/java/com/severin/baron/services_lab/MainActivity.java
+++ our-code/services-lab/app/src/main/java/com/severin/baron/services_lab/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.android.gms.awareness.Awareness;
 import com.google.android.gms.common.ConnectionResult;
@@ -13,11 +14,16 @@ import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,GoogleApiClient.ConnectionCallbacks {
     GoogleApiClient mGoogleApiClient;
+    Button activityButton, weatherButton, nearbyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activityButton = (Button) findViewById(R.id.activity_button);
+        weatherButton = (Button) findViewById(R.id.weather_button);
+        nearbyButton = (Button) findViewById(R.id.nearby_button);
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this,this)
