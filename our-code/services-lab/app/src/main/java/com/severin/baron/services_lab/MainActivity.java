3e4cfeec84babcbb0701053dc3ab7e6687865f65
Matthew Duffin <matthewtduffin@gmail.com>
2016-08-16 12:41:32 -0700

    added in some methods

--- our-code/services-lab/app/src/main/java/com/severin/baron/services_lab/MainActivity.java
+++ our-code/services-lab/app/src/main/java/com/severin/baron/services_lab/MainActivity.java
@@ -1,11 +1,15 @@
 package com.severin.baron.services_lab;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.awareness.Awareness;
 import com.google.android.gms.common.ConnectionResult;
@@ -15,6 +19,10 @@ import com.google.android.gms.location.LocationServices;
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,GoogleApiClient.ConnectionCallbacks {
     GoogleApiClient mGoogleApiClient;
     Button activityButton, weatherButton, nearbyButton;
+
+    public static final int ACTIVITY_CODE = 123;
+    public static final int WEATHER_CODE = 128;
+    public static final int NEARBY_CODE = 137;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,29 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case NEARBY_CODE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getNearbyPlaces();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+        }
+    }
+
+    private void getNearbyPlaces(){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, NEARBY_CODE);
+            return;
+        }
+    }
+
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
