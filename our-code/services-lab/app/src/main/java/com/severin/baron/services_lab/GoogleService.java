e36e36d0088d8e847fd30171534d17b4e8ecc7ae
MichaelKang <mgkang0206@gmail.com>
2016-08-16 20:28:19 -0700

    mani fixed, service class made with onBind

---
+++ our-code/services-lab/app/src/main/java/com/severin/baron/services_lab/GoogleService.java
@@ -0,0 +1,16 @@
+package com.severin.baron.services_lab;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by mgkan on 2016-08-16.
+ */
+public class GoogleService extends Service {
+  
+  @Override
+  public IBinder onBind(Intent intent) {
+    return null;
+  }
+}
--- our-code/services-lab/app/src/main/java/com/severin/baron/services_lab/GoogleServiceApp.java
+++
@@ -1,9 +0,0 @@
-package com.severin.baron.services_lab;
-
-import android.app.Application;
-
-/**
- * Created by mgkan on 2016-08-16.
- */
-public class GoogleServiceApp extends Application {
-}
